be14647c457c1b0a80ce57bb7173c1d2bba30615
Patrick Evers <pmdevers@gmail.com>
2021-08-31 11:10:04 +0200

    Working installer

---
+++ tools/Mockingjay-installer/Installer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Mockingjay_installer
+{
+    [RunInstaller(true)]
+    public class Installer : System.Configuration.Install.Installer
+    {
+        public override void Install(IDictionary stateSaver)
+        { 
+            base.Install(stateSaver);
+            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                
+                System.IO.File.Create("test");
+
+                //string path = Context.Parameters["targetdir"];
+                //RegistryKey myKey = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment", true);
+                //if (myKey != null)
+                //{
+                //    var old = myKey.GetValue("Path");
+                //    myKey.SetValue("Path", old + ";" + path, RegistryValueKind.String);
+                //    myKey.Close();
+                //}
+            }
+           
+        }
+    }
+}
